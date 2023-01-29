a6e4d463be2adcd51b09a8e16b1cd4e99b536aec
Davi Selio <davi.selio@signavio.com>
2023-01-29 18:31:56 +0100

    Minor adjustments for chapter 1

--- Chapter01/src/main/java/dev/davivieira/application/ports/input/RouterViewInputPort.java
+++ Chapter01/src/main/java/dev/davivieira/application/ports/input/RouterViewInputPort.java
@@ -4,8 +4,6 @@ import dev.davivieira.application.ports.output.RouterViewOutputPort;
 import dev.davivieira.application.usecases.RouterViewUseCase;
 import dev.davivieira.domain.Router;
 
-import static dev.davivieira.domain.Router.retrieveRouter;
-
 import java.util.List;
 import java.util.function.Predicate;
 
