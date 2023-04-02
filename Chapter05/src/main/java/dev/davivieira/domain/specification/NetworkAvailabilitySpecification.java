1c8dd69b20996276bfcadc9a23ede94a7aaf8efa
Davi Vieira <davi@deliverance.com.br>
2023-04-03 01:18:12 +0200

    Add code for chapter 5

---
+++ Chapter05/src/main/java/dev/davivieira/domain/specification/NetworkAvailabilitySpecification.java
@@ -0,0 +1,34 @@
+package dev.davivieira.domain.specification;
+
+import dev.davivieira.domain.entity.Router;
+import dev.davivieira.domain.vo.IP;
+import dev.davivieira.domain.vo.Network;
+
+public final class NetworkAvailabilitySpecification extends AbstractSpecification<Router> {
+
+    private final IP address;
+    private final String name;
+    private final int cidr;
+
+    public NetworkAvailabilitySpecification(IP address, String name, int cidr) {
+        this.address = address;
+        this.name = name;
+        this.cidr = cidr;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(Router router) {
+        return router!=null && isNetworkAvailable(router);
+    }
+
+    private boolean isNetworkAvailable(Router router) {
+        var availability = true;
+        for (Network network : router.retrieveNetworks()) {
+            if (network.getAddress().equals(address) && network.getName().equals(name) && network.getCidr() == cidr) {
+                availability = false;
+                break;
+            }
+        }
+        return availability;
+    }
+}
